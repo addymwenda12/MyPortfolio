30137aa2f28e8373ff84408b2e19ebf7c6203227
addymwenda12 <muneneaddy@gmail.com>
2024-07-22 18:51:53 +0300

    chore: Add LoadingScreen component
    The LoadingScreen component is added to display a loading screen with a random greeting. The useRandomGreeting hook is used to fetch a random greeting, and the LanguageGreeting component is rendered with the fetched greeting.

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRandomGreeting } from "../hooks/useRandomGreeting";
+import LanguageGreeting from './LanguageGreeting';
+
+const LoadingScreen: React.FC = () => {
+  const greeting = useRandomGreeting();
+
+  return (
+    <div className="loading-screen">
+      <LanguageGreeting greeting={greeting} />
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default LoadingScreen;
