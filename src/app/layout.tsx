4cd20227331823d723afd131713f6e7c6085ba11
addymwenda12 <muneneaddy@gmail.com>
2024-07-22 18:58:27 +0300

    feat: Add RootLayout component for wrapping the application content
    The RootLayout component is added to provide a common layout structure for the application. It includes the necessary HTML structure and imports the global CSS file. The metadata object is also defined to provide title and description for the page.
    
    Co-authored-by: Addy Mutuiri <muneneaddy@gmail.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './global.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Addy Mutuiri',
+  description: 'A showcase of my works and skills',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='eng'>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
