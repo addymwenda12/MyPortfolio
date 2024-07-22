7d69587361be32c2d43a2c07fd12ccb85c11c1e0
addymwenda12 <muneneaddy@gmail.com>
2024-07-22 18:53:58 +0300

    feat: Add LanguageGreeting component for displaying greetings in different languages

---
+++ src/components/LanguageGreeting.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Greetings } from "../lib/greetings";
+
+interface LanguageGreetingProps {
+  greeting: Greetings;
+}
+
+const LanguageGreeting: React.FC<LanguageGreetingProps> = ({ greeting }) => {
+  return (
+    <div className="language-greeting">
+      <h2>{greeting.text}</h2>
+      <p>{greeting.language}</p>
+    </div>
+  );
+};
+
+export default LanguageGreeting;
