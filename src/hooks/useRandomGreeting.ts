ace140cb0f11a9e58e65c20f1fe978cbc0441201
addymwenda12 <muneneaddy@gmail.com>
2024-07-22 18:48:36 +0300

    feat: Add useRandomGreeting hook for displaying random greetings

---
+++ src/hooks/useRandomGreeting.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { greetings, Greetings } from "../lib/greetings";
+
+export function useRandomGreeting(interval: number = 1500) {
+  const [currentGreeting, setCurrentGreeting] = useState<Greetings>(greetings[0]);
+
+  useEffect(() => {
+    const changeGreeting = () => {
+      const randomIndex = Math.floor(Math.random() * greetings.length);
+      setCurrentGreeting(greetings[randomIndex]);
+    };
+
+    const timer = setInterval(changeGreeting, interval);
+
+    return () => clearInterval(timer);
+  }, [interval]);
+
+  return currentGreeting;
+}
