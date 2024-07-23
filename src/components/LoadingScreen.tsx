c7f92724def60aa3986b8f97018b7fb72bc1b9a7
addymwenda12 <muneneaddy@gmail.com>
2024-07-23 16:25:27 +0300

    Rendering loading screen to the website

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -8,9 +8,27 @@ const LoadingScreen: React.FC = () => {
   const greeting = useRandomGreeting();
 
   return (
-    <div className="loading-screen">
+    <div className="loading-screen" style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f0f0f0',
+    }}>
       <LanguageGreeting greeting={greeting} />
-      <div className="loader"></div>
+      <div className="loader" style={{
+        width: '50px',
+        height: '50px',
+        border: '5px solid #333',
+        borderTop: '5px solid #3498db',
+        borderRadius: '50%',
+        animation: 'spin 1s linear infinite'
+      }}></div>
     </div>
   );
 }
