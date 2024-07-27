d39f4e2a5a817a6221ee855d7d627d49a4fe79fd
addymwenda12 <muneneaddy@gmail.com>
2024-07-27 18:38:46 +0300

    Created home page for the portfolio

---
+++ src/app/hero/Hero.tsx
@@ -0,0 +1,127 @@
+import Link from "next/link";
+import Image from "next/image";
+import { chillax } from "../fonts/chillax";
+import { motion } from "framer-motion";
+import { imageAnimation, bodyAnimation } from "../animations/animations";
+import AnimatedWords from "../animations/AnimatedWords";
+// import profile from "../../public/profile.webp";
+
+const Hero = () => {
+  return (
+    <motion.section
+      className="relative z-10 flex h-[80vh] w-full items-stretch justify-center bg-[url('.//../public/hero.jpg')] bg-cover  bg-center py-0 sm:h-[90vh]  md:h-[100vh]"
+      id="home"
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div className="absolute left-0 top-0 right-0 bottom-0 h-full w-full bg-[#0E1016] mix-blend-color"></motion.div>
+
+      <div className="absolute top-10 flex justify-between sm:w-[90%] lg:max-w-[1440px]">
+        <div>
+          <Link
+            href="https://drive.google.com/file/d/1aCxUde_o-3DrfBEY5wPzJ-MWcaFtSKQh/view?usp=sharing"
+            target="_blank"
+            aria-label="View Resume"
+          >
+            <motion.button
+              className="hidden rounded-md border-2 border-[#e4ded7] py-2 px-4 text-[14px] font-semibold text-[#e4ded7] sm:block  md:text-[16px] lg:block"
+              variants={bodyAnimation}
+            >
+              VIEW RESUME
+            </motion.button>
+          </Link>
+        </div>
+
+        <div className="flex gap-10 text-[#e4ded7] sm:gap-12 md:gap-14 lg:gap-14">
+          <Link
+            href="https://github.com/addymwenda12"
+            target="_blank"
+            aria-label="View GitHub Profile"
+          >
+            <motion.p
+              className="text-[16px] font-bold text-[#e4ded7] md:text-[16px]"
+              variants={bodyAnimation}
+            >
+              GH
+            </motion.p>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/addymuturi"
+            target="_blank"
+            aria-label="View LinkedIn Profile"
+          >
+            <motion.p
+              className="text-[16px] font-bold text-[#e4ded7] md:text-[16px]"
+              variants={bodyAnimation}
+            >
+              LN
+            </motion.p>
+          </Link>
+          <Link
+            href="https://x.com/AddyMwenda"
+            target="_blank"
+            aria-label="View X Profile"
+          >
+            <motion.p
+              className="text-[16px] font-bold text-[#e4ded7] md:text-[16px]"
+              variants={bodyAnimation}
+            >
+              X
+            </motion.p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="-mt-36 flex flex-col items-center justify-center sm:-mt-20 lg:my-40 lg:-mt-2 lg:py-40 ">
+        <div
+          className={`relative flex flex-col items-center justify-center ${chillax.className}   `}
+        >
+          <AnimatedWords
+            title="ADDY MUTUIRI"
+            style="inline-block overflow-hidden pt-1 -mr-4 sm:-mr-5 md:-mr-7 lg:-mr-9 -mb-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
+          />
+          <motion.div
+            className="absolute bottom-[-110px] mx-auto sm:bottom-[-100px] md:bottom-[-130px] lg:bottom-[-150px]"
+            variants={imageAnimation}
+          >
+            <Image
+              src={profile}
+              priority
+              alt="Victor's headshot"
+              data-blobity-tooltip="Fine boy"
+              data-blobity-invert="false"
+              className=" w-[150px] rounded-[16px] grayscale hover:grayscale-0 md:w-[200px] md:rounded-[32px] lg:w-[245px]"
+            />
+          </motion.div>
+        </div>
+      </div>
+
+      <div
+        className="absolute bottom-10 flex items-center 
+      justify-center
+      md:bottom-10 lg:w-[90%] lg:max-w-[1440px] lg:justify-between"
+      >
+        <motion.div
+          className="  max-w-[350px] md:max-w-[400px] lg:max-w-[400px]"
+          variants={bodyAnimation}
+        >
+          <p className="z-50 text-center text-[16px] font-medium text-[#e4ded7] md:text-[20px] lg:text-left">
+            Backend specialist and Web developer, ALX and Headstarter AI fellow
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="  hidden max-w-[500px] lg:block lg:max-w-[420px]"
+          variants={bodyAnimation}
+        >
+          <p className="text-right text-[16px] font-semibold text-[#e4ded7] md:text-[20px]">
+            Focused on servers and developments, working remotely from Nairobi,
+            Kenya.
+          </p>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Hero;
