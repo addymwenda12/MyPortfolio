001a417f8cfb6a39de686d3636cef5426067e23b
addymwenda12 <muneneaddy@gmail.com>
2024-07-27 18:25:00 +0300

    Added font style

---
+++ src/app/fonts/chillax.ts
@@ -0,0 +1,71 @@
+import localFont from "next/font/local";
+
+export const chillax = localFont({
+  src: [
+    {
+      path: "./Chillax-ExtraLight.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Extralight.woff2",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "./Chillax-Semibold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Semibold.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./Chillax-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Light.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./Chillax-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Medium.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./Mona-Sans-SemiBoldNarrow.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./Mona-Sans-SemiBoldNarrowItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./Chillax-Regular.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Regular.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./Chillax-Regular.woff2",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+});
